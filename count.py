7b87c0548c54e1f42afcce3feeb907946381ba0f
Vasily Yurikov <loney2ears@yandex.ru>
2022-11-14 15:06:22 +1000

    Major

--- count.py
+++ count.py
@@ -2,9 +2,16 @@ import os
 
 
 if __name__ == '__main__':
-    files_amount = 0
+    ok_files_amount = 0
     folders = os.listdir(path=".")
     for folder in folders:
-        if os.path.isdir(folder) and '.' not in folder:
-            files_amount += len(os.listdir(path=f"./{folder}"))
-    print(f'{files_amount = }')
+        if os.path.isdir(folder) and '.' not in folder and folder != 'LOST':
+            ok_files_amount += len(os.listdir(path=f"./{folder}"))
+    lost_files_amount = 0
+    folders = os.listdir(path="LOST")
+    for folder in folders:
+        if os.path.isdir(f"LOST/{folder}"):
+            lost_files_amount += len(os.listdir(path=f"LOST/{folder}"))
+    print(f'TOTAL = {ok_files_amount + lost_files_amount}')
+    print(f'{ok_files_amount = }')
+    print(f'{lost_files_amount = }')
